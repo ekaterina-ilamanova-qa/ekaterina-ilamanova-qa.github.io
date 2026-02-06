033b31a2000b2230af550cfd1abdeafe7930337a
ekaterina-ilamanova-qa <ekaterinaandreevna19@gmail.com>
2026-02-06 23:26:05 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="./normalize.css" rel="stylesheet" />
+    <link href="./style.css" rel="stylesheet" />
+    <title>Document</title>
+  </head>
+  <body>
+    <header class="header">
+      <img src="./avatar.png" alt="avatar" class="header__avatar" />
+      <h1 class="header__title">Екатерина Иламанова</h1>
+      <h3 class="header__subtitle">work hard travel hard</h3>
+    </header>
+    <main>
+      <section class="links">
+        <h2 class="links__header">Доступна</h2>
+        <ul class="links__container">
+          <li class="links__link">
+            <p class="links__title">Телеграм</p>
+            <a class="linsk__link" href="https://t.me/KaterinaAndreevnaI">@KaterinaAndreevnaI</a>
+          </li>
+          <li class="links__link">
+            <p class="links__title">Эл. почта</p>
+            <a class="linsk__link" href="mailto:ilamanova.ekaterina@yandex.com"
+              >ilamanova.ekaterina@ya.com</a
+            >
+          </li>
+          <li class="links__link">
+            <p class="links__title">Моб. телефон</p>
+            <a class="linsk__link" href="tel:+79150116163"
+              >+79150116163</a
+            >
+          </li>
+        </ul>
+      </section>
+      <section class="links">
+        <h2 class="links__header">Чем занимаюсь вечером</h2>
+        <ul class="links__container">
+          <li class="links__link">
+            <p class="links__title">Хобби</p>
+            <a class="linsk__link">Организую поездки по всему миру</a>
+          </li>
+          <li class="links__link">
+            <p class="links__title">Работаю</p>
+            <a class="linsk__link">над собой</a>
+          </li>
+          <li class="links__link">
+            <p class="links__title">Учусь новому</p>
+            <a class="linsk__link">«Учитесь постоянно: всегда есть ещё одна вещь, которую нужно изучить» — Стив Джобс 
+</a>
+          </li>
+        </ul>
+      </section>
+    </main>
+    <footer>
+      <button type = "button" class="button-link" href=""> Кредо </button>
+    </footer>
+    <script type="text/javascript" src="js.js"></script>
+  </body>
+</html>
